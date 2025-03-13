96e698075009407b6b0b908ef6e3c7e9dd611f2a
Julius Banda <bandahmj@gmail.com>
2025-03-13 09:04:49 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/zm/co/roadguard/MainActivity.kt
@@ -0,0 +1,6 @@
+package zm.co.roadguard
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:road_guard/app/app.dart';
+import 'package:road_guard/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
